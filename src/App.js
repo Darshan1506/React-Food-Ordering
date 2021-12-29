3919b2c28f76594a034c6acad2002dc8522b6045
Darshan <darshanrajpurohit47@gmail.com>
2021-12-29 18:31:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './components/store/CartProvider';
+
+function App() {
+  const [cartIsShown , SetCartIsShown] =  useState(false);
+  const showCartHandler = ()=>{
+    SetCartIsShown(true);
+  }
+  const hideCartHandler= ()=>{
+    SetCartIsShown(false);
+  }
+  return (
+    <CartProvider>
+      {cartIsShown&&<Cart onClose={hideCartHandler}/>}
+      <Header onShowCart ={showCartHandler}/>
+      <main>
+      <Meals/>
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,38 @@
+import { useContext , useEffect , useState } from "react";
+import CartContext from "../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) =>{
+    const [btnIsHighLighted , setBtnIsHighLighted] = useState(false);
+    const cartctx =useContext(CartContext);
+    const numberOfCartItems = cartctx.items.reduce((currNumber , item)=>{
+        return currNumber + item.amount;
+    },0);
+    const btnClasses = `${classes.button} ${ btnIsHighLighted ? classes.bump : ''}`;
+    const {items} = cartctx;
+    useEffect(()=>{
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighLighted(true);
+
+       const timer = setTimeout(()=>{
+            setBtnIsHighLighted(false);
+        },300);
+
+        return() =>{
+            clearTimeout(timer);
+        }
+    },[items]);
+    return (
+        <button className={btnClasses} onClick={props.onClick}>
+            <span className={classes.icon}>
+               <CartIcon/> 
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}> {numberOfCartItems}</span>
+        </button>
+    )
+
+}
+export default HeaderCartButton;
